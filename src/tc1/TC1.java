294c0e329586dbb586ddce5f8f10f5f5e817e68b
RamiaAbdo <abdomakhol@yahoo.com>
2022-02-03 22:18:45 -0500

    Stop tracking .iml

--- src/tc1/TC1.java
+++ src/tc1/TC1.java
@@ -3,6 +3,8 @@ package tc1;
 public class TC1 {
     public static void main(String[] args) {
         System.out.println("Print first msn");
+        System.out.println("share more commit on github");
+        System.out.println("share more commit ");
 
     }
 
